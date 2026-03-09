aac32319bbd08ec1dbff6ece9e4fb80a7755f51d
Koo Jozsef (XC-AS/EDP1-Bp) <Jozsef.Koo@hu.bosch.com>
2026-03-09 14:59:42 +0100

    following django tutorial part 2

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
